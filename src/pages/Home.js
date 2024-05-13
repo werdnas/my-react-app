6cf8e270c23e3309bad73c09ec1ddae805d88b9a
a3innoark <andrews.kubernetes@gmail.com>
2024-05-13 20:22:48 +0530

    home page update
    home page update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Home(props) {
-  return <div>Home Page - Github webhook</div>;
+  return <div>Home Page - Github webhook done</div>;
 }
 
 export default Home;
