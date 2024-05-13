8748543ca748ec1734302068f21c3e39b07841ec
a3innoark <andrews.kubernetes@gmail.com>
2024-05-13 12:27:58 +0530

    localhost integration
    localhost integration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Home(props) {
-  return <div>Home Page - Github webhook integration</div>;
+  return <div>Home Page - Github webhook localhost</div>;
 }
 
 export default Home;
